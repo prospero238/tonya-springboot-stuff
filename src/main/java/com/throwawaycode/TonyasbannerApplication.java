b39f156009c7968198010218be8f6edded8173ac
propspero238 <keithc238@gmail.com>
2017-01-25 18:30:12 -0800

    added king tut

---
+++ src/main/java/com/throwawaycode/TonyasbannerApplication.java
@@ -0,0 +1,12 @@
+package com.throwawaycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonyasbannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonyasbannerApplication.class, args);
+	}
+}
